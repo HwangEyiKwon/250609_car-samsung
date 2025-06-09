f98a8fb9645b3c6e4384fd3252b4b9ef11e1bd82
ek1103.hwang <hwang21035@naver.com>
2025-06-09 15:10:58 +0900

    Test Case 추가

--- mission2/250609_mission2_project/250609_mission2_project/assembly_car.cpp
+++ mission2/250609_mission2_project/250609_mission2_project/assembly_car.cpp
@@ -1,26 +1,153 @@
 #include "gmock/gmock.h"
 #include "car.h"
+#include "carValidator.h"
+#include "carBuilder.h"
 
 using namespace testing;
 
-class MockCar : public Car {
+class MockEngine : public Engine {
 public:
-	MOCK_METHOD(int, getSum, (int a, int b), ());
+    MOCK_METHOD(EngineName, getEngineName, (), (override));
 };
 
-TEST(Group, TC1) {
-	Car car;
+class MockBrakeSystem : public BrakeSystem {
+public:
+    MOCK_METHOD(BrakeSystemName, getBrakeSystemName, (), (override));
+};
 
-	int ret = car.getSum(10, 20);
+class MockSteeringSystem : public SteeringSystem {
+public:
+    MOCK_METHOD(SteeringSystemName, getSteeringSystemName, (), (override));
+};
 
-	EXPECT_EQ(30, ret);
+TEST(CarPartValidatorTest, TC1_SEDAN_CONTINENTAL) {
+    MockEngine engine;
+    MockBrakeSystem brakeSystem;
+    MockSteeringSystem steeringSystem;
+
+    CarType carType = SEDAN;
+    EXPECT_CALL(engine, getEngineName()).WillRepeatedly(Return(GM));
+    EXPECT_CALL(brakeSystem, getBrakeSystemName()).WillRepeatedly(Return(CONTINENTAL));
+    EXPECT_CALL(steeringSystem, getSteeringSystemName()).WillRepeatedly(Return(BOSCH_S));
+
+    ConcreteCarBuilder* builder = new ConcreteCarBuilder();
+    Car* car = builder->setCarType(carType)
+                        .setEngine(engine.getEngineName())
+                        .setBrakeSystem(brakeSystem.getBrakeSystemName())
+                        .setSteeringSystem(steeringSystem.getSteeringSystemName())
+                        .build();
+
+    bool result = CarValidator::validate(car);
+
+    EXPECT_FALSE(result);
 }
 
-TEST(Group, TC2) {
-	MockCar mock;
+TEST(CarPartValidatorTest, TC2_SUV_TOYOTA) {
+    MockEngine engine;
+    MockBrakeSystem brakeSystem;
+    MockSteeringSystem steeringSystem;
 
-	EXPECT_CALL(mock, getSum(1, 2)).WillRepeatedly(Return(999));
+    CarType carType = SUV;
+    EXPECT_CALL(engine, getEngineName()).WillRepeatedly(Return(TOYOTA));
+    EXPECT_CALL(brakeSystem, getBrakeSystemName()).WillRepeatedly(Return(CONTINENTAL));
+    EXPECT_CALL(steeringSystem, getSteeringSystemName()).WillRepeatedly(Return(BOSCH_S));
 
-	int ret = mock.getSum(1, 2);
-	EXPECT_EQ(999, ret);
+    ConcreteCarBuilder* builder = new ConcreteCarBuilder();
+    Car* car = builder->setCarType(carType)
+        .setEngine(engine.getEngineName())
+        .setBrakeSystem(brakeSystem.getBrakeSystemName())
+        .setSteeringSystem(steeringSystem.getSteeringSystemName())
+        .build();
+
+    bool result = CarValidator::validate(car);
+
+    EXPECT_FALSE(result);
+}
+
+TEST(CarPartValidatorTest, TC3_TRUCK_WIA) {
+    MockEngine engine;
+    MockBrakeSystem brakeSystem;
+    MockSteeringSystem steeringSystem;
+
+    CarType carType = TRUCK;
+    EXPECT_CALL(engine, getEngineName()).WillRepeatedly(Return(WIA));
+    EXPECT_CALL(brakeSystem, getBrakeSystemName()).WillRepeatedly(Return(CONTINENTAL));
+    EXPECT_CALL(steeringSystem, getSteeringSystemName()).WillRepeatedly(Return(BOSCH_S));
+
+    ConcreteCarBuilder* builder = new ConcreteCarBuilder();
+    Car* car = builder->setCarType(carType)
+        .setEngine(engine.getEngineName())
+        .setBrakeSystem(brakeSystem.getBrakeSystemName())
+        .setSteeringSystem(steeringSystem.getSteeringSystemName())
+        .build();
+
+    bool result = CarValidator::validate(car);
+
+    EXPECT_FALSE(result);
+}
+
+TEST(CarPartValidatorTest, TC4_TRUCK_MANDO) {
+    MockEngine engine;
+    MockBrakeSystem brakeSystem;
+    MockSteeringSystem steeringSystem;
+
+    CarType carType = TRUCK;
+    EXPECT_CALL(engine, getEngineName()).WillRepeatedly(Return(TOYOTA));
+    EXPECT_CALL(brakeSystem, getBrakeSystemName()).WillRepeatedly(Return(MANDO));
+    EXPECT_CALL(steeringSystem, getSteeringSystemName()).WillRepeatedly(Return(BOSCH_S));
+
+    ConcreteCarBuilder* builder = new ConcreteCarBuilder();
+    Car* car = builder->setCarType(carType)
+        .setEngine(engine.getEngineName())
+        .setBrakeSystem(brakeSystem.getBrakeSystemName())
+        .setSteeringSystem(steeringSystem.getSteeringSystemName())
+        .build();
+
+    bool result = CarValidator::validate(car);
+
+    EXPECT_FALSE(result);
+}
+
+TEST(CarPartValidatorTest, TC5_BOSCH_DUO) {
+    MockEngine engine;
+    MockBrakeSystem brakeSystem;
+    MockSteeringSystem steeringSystem;
+
+    CarType carType = TRUCK;
+    EXPECT_CALL(engine, getEngineName()).WillRepeatedly(Return(TOYOTA));
+    EXPECT_CALL(brakeSystem, getBrakeSystemName()).WillRepeatedly(Return(BOSCH_B));
+    EXPECT_CALL(steeringSystem, getSteeringSystemName()).WillRepeatedly(Return(MOBIS));
+
+    ConcreteCarBuilder* builder = new ConcreteCarBuilder();
+    Car* car = builder->setCarType(carType)
+        .setEngine(engine.getEngineName())
+        .setBrakeSystem(brakeSystem.getBrakeSystemName())
+        .setSteeringSystem(steeringSystem.getSteeringSystemName())
+        .build();
+
+    bool result = CarValidator::validate(car);
+
+    EXPECT_FALSE(result);
+}
+
+TEST(CarPartValidatorTest, TC6_BROKEN_ENGINE) {
+    MockEngine engine;
+    MockBrakeSystem brakeSystem;
+    MockSteeringSystem steeringSystem;
+
+    CarType carType = SEDAN;
+    EXPECT_CALL(engine, getEngineName()).WillRepeatedly(Return(BROKEN));
+    EXPECT_CALL(brakeSystem, getBrakeSystemName()).WillRepeatedly(Return(BOSCH_B));
+    EXPECT_CALL(steeringSystem, getSteeringSystemName()).WillRepeatedly(Return(BOSCH_S));
+
+    ConcreteCarBuilder* builder = new ConcreteCarBuilder();
+    Car* car = builder->setCarType(carType)
+        .setEngine(engine.getEngineName())
+        .setBrakeSystem(brakeSystem.getBrakeSystemName())
+        .setSteeringSystem(steeringSystem.getSteeringSystemName())
+        .build();
+
+    bool result = CarValidator::validate(car);
+
+    EXPECT_TRUE(result);
 }
